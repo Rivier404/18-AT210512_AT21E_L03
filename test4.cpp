3ffb07b7467aa64563f460a007dbb9c50ad22b67
Rivier404 <phamduc13122006@gmail.com>
2025-03-06 10:29:30 +0700

    b4.cpp

--- test4.cpp
+++ test4.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main (){
 	float r;
+	printf("nhap ban kinh cua hinh cau: ");
 	scanf("%f",&r);
-	printf("%f\n",4*3.14*r*r);
-	printf("%f\n",4.0/3.0*3.14*r*r*r);
+	printf("dien tich hinh cau la: %f\n",4*3.14*r*r);
+	printf("the tich hinh cau la: %f\n",4.0/3.0*3.14*r*r*r);
 }
