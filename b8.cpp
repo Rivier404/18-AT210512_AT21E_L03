f7486e618a15f469dbca7b7547a3bde0c1237b0c
Rivier404 <phamduc13122006@gmail.com>
2025-03-06 17:10:55 +0700

    Update b8.cpp

--- b8.cpp
+++ b8.cpp
@@ -12,29 +12,41 @@
 	    if (a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a) return 4;
 	    return 5;
 	}
-	float dientichchuvi (float a, float b, float c) {
+	float dientich (float a, float b, float c) {
 		float p=(a+b+c)/2.0;
-		return sqrt(p*(p-a)(p-b)*(p-c));
+		return sqrt(p*(p-a)*(p-b)*(p-c));
 	}
 	
 	int main (){	
-		float loai=loaitamgiac(a,b,c);
+	
+        float a,b,c;
+        	
         printf("nhap vao 3 canh cua tam giac: ");
 	scanf("%f%f%f",&a,&b,&c);
-	switch (loai)
+if( !tamgiac(a,b,c)) {
+	printf("khong ton tai tam giac");
+	return 1;
+		}
+		int loai= loaitamgiac(a,b,c);
+	switch (loai){
 		case 1:
-printf("tam giac deu");
+printf("tam giac deu\n");
 break;
 case 2:
-printf("tam giac can");
+printf("tam giac can\n");
 	break;
 	case 3:
-	printf("tam giac vuong can");
+	printf("tam giac vuong can\n");
 	break;
 	case 4:
-	printf("tam giac vuong");
+	printf("tam giac vuong\n");
+		break;
 case 5:
-printf("tam giac thuong");
+printf("tam giac thuong\n");
 break;
+	}
+		printf("Chu vi tam giac la: %.2f\n",a+b+c);
+		printf("Dien tich tam giac la:%.2f", dientich(a,b,c));
+			
 }
-}
+
