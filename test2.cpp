4d0b4a7a56b5b8cd74f82943518774ddaded0b79
Rivier404 <phamduc13122006@gmail.com>
2025-03-06 10:21:31 +0700

    b2.ccp

--- test2.cpp
+++ test2.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main (){
 	int ngay,thang,nam;
+	printf("nhap lan luot ngay thang nam");
 	scanf("%d%d%d",&ngay,&thang,&nam);
+	if(ngay <= 0 || ngay >31) {
+		printf("???");
+	}
+	if (thang <=0 || thang >12){
+		printf("????");
+	}
 	printf("hom nay la ngay %d/%d/%d",ngay,thang,nam);
 }
