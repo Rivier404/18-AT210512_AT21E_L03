380a9987c149f69e47624ea0d01d1ebbf7c14237
Rivier404 <phamduc13122006@gmail.com>
2025-03-06 11:11:04 +0700

    Update and rename test4.cpp to b4.cpp

---
+++ b4.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+	float r;
+	printf("nhap ban kinh cua hinh cau: ");
+	scanf("%f",&r);
+	printf("dien tich hinh cau la: %.2f\n",4*3.14*r*r);
+	printf("the tich hinh cau la: %.2f\n",4.0/3.0*3.14*r*r*r);
+	
+}
--- test4.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-	float r;
-	printf("nhap ban kinh cua hinh cau: ");
-	scanf("%f",&r);
-	printf("dien tich hinh cau la: %f\n",4*3.14*r*r);
-	printf("the tich hinh cau la: %f\n",4.0/3.0*3.14*r*r*r);
-	
-}
