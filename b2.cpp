eb3c69f80a7865524023a46b3c0ab7a5ae33f70e
Rivier404 <phamduc13122006@gmail.com>
2025-03-07 17:00:20 +0700

    final????

--- b2.cpp
+++ b2.cpp
@@ -11,7 +11,27 @@ if(ngay <= 0 || ngay >31) {
 		printf("???");
 		return 0;
 	}
-         int ngay_max;
-	switch 
+         int max;
+	switch (thang) {
+		case 2:
+		if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0)) {
+			max=29;
+		}
+			else {
+				max =28;
+			}
+			break;
+		
+		case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+		max=31;
+		break;
+		case 4:case 6: case 9:case 11:
+		max=30;
+		break;
+	}
+	if (ngay>max){
+		printf("????");
+		return 0;
+	}
 	printf("hom nay la ngay %d/%d/%d",ngay,thang,nam);
 }
