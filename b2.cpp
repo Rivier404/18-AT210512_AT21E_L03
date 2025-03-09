9f0d2872b5e10837751dd9a4f37e10bbcb9794a8
Rivier404 <phamduc13122006@gmail.com>
2025-03-09 09:41:09 +0700

    Update and rename test2.cpp to b2.cpp

--- b2.cpp
+++ b2.cpp
@@ -3,13 +3,6 @@ int main (){
 	int ngay,thang,nam;
 	printf("nhap lan luot ngay thang nam");
 	scanf("%d%d%d",&ngay,&thang,&nam);
-	if(ngay <= 0 || ngay >31) {
-		printf("???");
-		return 0;
-	}
-	if (thang <=0 || thang >12){
-		printf("????");
-		return 0;
-	}
+	
 	printf("hom nay la ngay %d/%d/%d",ngay,thang,nam);
 }
